f1c048ce39e6075075fb9fa87f2b0aff3355776c
biskilled <tal@biskilled.com>
2020-02-06 17:04:39 +0200

    yallla

--- test/test.py
+++
@@ -1,8 +0,0 @@
-from collections import OrderedDict
-
-class x ():
-    def __init__ (self,p=None, **args):
-        print (args)
-
-
-xx = x ()
--- test/testMain.py
+++
@@ -1,6 +0,0 @@
-from lib.dingDONG.conn.connGlobalDB import connGlobalDb
-from dingDONG.misc.enums            import eConn
-
-dt = {eConn.dataTypes.DB_DATE:['dddddddddddddd']}
-
-xx = connGlobalDb (connType='sql', connUrl='fdfdfdfdf', dataTypes=dt)
